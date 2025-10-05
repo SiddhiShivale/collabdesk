7617be63adfc28243db4a60764577271dae55617
Siddhi <siddhishivale1@gmail.com>
2025-10-05 22:59:13 +0530

    Initial commit

---
+++ src/main/java/com/collabdesk/app/CollabDeskApplication.java
@@ -0,0 +1,13 @@
+package com.collabdesk.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabDeskApplication.class, args);
+	}
+
+}
